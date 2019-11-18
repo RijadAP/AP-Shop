1f50c5798bec9362594ecca6c19752d2de82b177
RijadAP <rijad.logo@authoritypartners.com>
2019-11-18 23:52:37 +0100

    Started woeking on BLL

---
+++ Backend/API/BLL/Classes/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL
+{
+    class User : IUser
+    {
+        //private IUserMenager
+        //public bool CheckIfUSerExists(string username)
+        //{
+
+        //}
+    }
+}
